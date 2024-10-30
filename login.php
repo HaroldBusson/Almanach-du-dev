eca739ac693952e4354edef3cac0b2ef912bda9b
Harold <harolddev75@gmail.com>
2024-10-30 17:07:30 +0100

    loops fixed

--- login.php
+++ login.php
@@ -23,13 +23,13 @@
 
 $conn = new mysqli("localhost", "root", "", "Gazettedonkey");
 
-if ((!isset$_POST($login) && !isset$_POST($password))){  // Si le login et le mot de passe envoyés sont corrects
+if (isset($_POST['login']) && isset($_POST['password'])) {  // Si le login et le mot de passe envoyés sont corrects
     echo "Connexion réussie Bienvenue sur votre réseau social"; 
 }else{
-    if(empty($_POST($login))){ // Si login envoyé est vide
+    if(empty($_POST('login'))){ // Si login envoyé est vide
         echo "Erreur ! Veuillez saisir votre login. \n"; 
     }else{
-        if (empty($_POST($password))) // si le mot de passe envoyé est vide
+        if (empty($_POST['password'])) { // si le mot de passe envoyé est vide
     } echo "Erreur ! Veuillez sasir votre mot de passe. \n";
 }
 
@@ -38,9 +38,9 @@ $sql = "SELECT name, email, firstname FROM user WHERE  = $utilisateur_id";
 
     if ($result->num_rows > 0) {
         // Affichage des données de l'utilisateur
-    while ($row = $result->fetch_assoc())
-     } echo "Nom: " . $row["nom"]. " - Email: " . $row["email"]. \n" ;
-                                            <br>
+    while ($row = $result->fetch_assoc()){ 
+     echo "Nom: " . $row["nom"]. " - Email: " . $row["email"] . "\n";
+        }
     } else {
         echo 'Aucun utilisateur trouvé avec cet ID . \n';
     }
