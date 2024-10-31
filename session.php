597ffdbe16f5cb32d4a3375b8fb3f3f98287342a
Harold <harolddev75@gmail.com>
2024-10-31 12:32:24 +0100

    indentation fixed

--- session.php
+++ session.php
@@ -1,6 +1,6 @@
-<?php    
-        /* page: session.php */
-    // Si le formulaire est soumis, créer les cookies
+<?php
+/* page: session.php */
+// Si le formulaire est soumis, créer les cookies
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { // vérifie si la requête a été envoyé en methode post
     setcookie('username', $_POST['username'], time() + 3600); // Il stocke la valeur du champ username du formulaire pendant 1 heure
     setcookie('', $_POST['password'], time() + 3600); // Il stocke la valeur du champ password du formulaire pendant 1 heure
@@ -14,7 +14,6 @@ if (isset($_POST['reset'])) {   // vérifie si le formulaire a été soumis avec
     setcookie('password', '', time() - 3600); // les cookies password en le définissant avec une valeur vide ('') et une date d'expiration dans le passé
     header("Location: index.php " . $_SERVER['PHP_SELF']);
     exit();
+} else {
+    $error = "Nom d'utilisateur ou mot de passe incorrect.";
 }
-
-?>
-  
